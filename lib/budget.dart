4b44bdb273c72fcaa4c116fe5c69fc732847ab5c
Jonathan Lee <thenameless0n3@gmail.com>
2024-10-16 16:40:16 -0700

    Added resetInterval to budget

--- lib/budget.dart
+++ lib/budget.dart
@@ -1,13 +1,25 @@
 /// This is the Budget class.
 /// It contains the label, remaining amount, and total amount of the budget.
 /// It also has methods to reset the budget, spend money, and get the percentage remaining.
+enum ResetInterval {
+  yearly,
+  monthly,
+  weekly,
+  daily,
+}
+
 class Budget {
   String label;
   double remaining;
   double total;
 
+  DateTime resetDate;
+  ResetInterval resetInterval;
+
   Budget(this.label, this.total, [double? remaining])
-      : remaining = remaining ?? total;
+      : remaining = remaining ?? total,
+        resetDate = DateTime(DateTime.now().year, DateTime.now().month + 1),
+        resetInterval = ResetInterval.monthly;
 
   void resetBudget() {
     remaining = total;
@@ -34,14 +46,39 @@ class Budget {
     }
   }
 
+  Duration getTimeRemaining() {
+    return resetDate.difference(DateTime.now());
+  }
+
+  void setResetDate() {
+    switch (resetInterval) {
+      case ResetInterval.yearly:
+        resetDate = DateTime(resetDate.year + 1, resetDate.month);
+        break;
+      case ResetInterval.monthly:
+        resetDate = DateTime(resetDate.year, resetDate.month + 1);
+        break;
+      case ResetInterval.weekly:
+        resetDate = DateTime(resetDate.year, resetDate.month, resetDate.day + 7);
+        break;
+      case ResetInterval.daily:
+        resetDate = DateTime(resetDate.year, resetDate.month, resetDate.day + 1);
+        break;
+    }
+  }
+
   Budget.fromJson(Map<String, dynamic> json)
       : label = json['label'] as String,
         remaining = json['remaining'] as double,
-        total = json['total'] as double;
+        total = json['total'] as double,
+        resetDate = DateTime.parse(json['resetDate'] as String),
+        resetInterval = ResetInterval.values[json['resetInterval'] as int];
 
   Map<String, dynamic> toJson() => {
         'label': label,
         'remaining': remaining,
         'total': total,
+        'resetDate': resetDate.toIso8601String(),
+        'resetInterval': resetInterval.index,
       };
 }
